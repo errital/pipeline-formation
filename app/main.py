e478bf73abbb633fb2f177bbf91e005192220ffe
errital <mohammed.erritali@iutbayonne.univ-pau.fr>
2026-09-10 16:13:55 +0200

    amodif main

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 
 from app.config import settings
 from app.routes import health, items, version
-
+#api fast
 app = FastAPI(
     title="Pipeline Craft API",
     description="API fil rouge pour la formation GitLab CI/CD",
